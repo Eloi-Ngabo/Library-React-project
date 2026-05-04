a9638c7243b7b8206319641bec3c028e79b8fcc1
Eloi-Ngabo <eloingabonziza@gmail.com>
2026-05-04 18:51:49 -0500

    new

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import Nav from './components/Nav';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './pages/Home';
 import Books from './pages/Books'
 import { books } from "./data";
@@ -15,9 +15,21 @@ function App() {
     <Router>
     <div className="App">
       <Nav />
-      <Route path="/" element component={Home} />
+
+      
+      {/* <Route path="/" element ={<Home />} />
       <Route path="/books" render={() => <Books books={books} />} />
-      <Route path="/books" render={() => <BookInfo books={books} />} />
+      <Route path="/books/1" render={() => <BookInfo books={books} />} /> 
+        */}
+
+      <Switch>
+  <Route exact path="/" render={() => <Home />} />
+  <Route exact path="/books" render={() => <Books books={books} />} />
+  <Route path="/books/:id" render={() => <BookInfo books={books} />} />
+</Switch>  
+        
+     
+
       <Footer />
     </div>
     </Router>
